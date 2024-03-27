396a1e0eb066b8729049dd4a253c3c64e78c7a6d
BaDong <dongba352001@gmail.com>
2024-03-27 16:03:40 +0700

    done init

--- js/count-time.js
+++ js/count-time.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const startDate = new Date('2024-03-23');
-    const currentDate = new Date();
+    function updateCountdown() {
+        const startDate = new Date(2024, 2, 23, 21, 0, 0);
+        const now = new Date();
+        const difference = now - startDate;
 
-    // Tính toán số ngày
-    const differenceInTime = currentDate.getTime() - startDate.getTime();
-    const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
-
-    // Cập nhật số ngày lên trên trang web
-    document.getElementById('countdown').innerText = differenceInDays > 0 ? differenceInDays + " ngày" : "Chưa đến ngày 23/03/2024";
+        if (difference > 0) {
+            const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    
+            document.getElementById('days').innerText = days;
+            document.getElementById('hours').innerText = hours;
+            document.getElementById('minutes').innerText = minutes;
+            document.getElementById('seconds').innerText = seconds;
+        } else {
+            document.getElementById('countdown').innerText = 'Chưa đến ngày 23/03/2024';
+        }
+    }
+    setInterval(updateCountdown, 1000);
 });
